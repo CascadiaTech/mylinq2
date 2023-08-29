b059577b896b6ac3f0966888ee49baede54f0f3b
alyaxi <ali.zaman@quanrio.com>
2023-08-29 06:32:03 +0500

    Updated UI and functionalities

--- components/Stake/PerpetualStacking.tsx
+++ components/Stake/PerpetualStacking.tsx
@@ -3,7 +3,7 @@ import React from "react";
 const PerpetualStacking = () => {
   return (
     <>
-      <div className="py-6 px-4 sm:p-10 mt-5 sm:mt-10 md:mt-10 lg:mt-15  w-[350px] sm:w-[350px] md:w-[550px]  min-h-[450px] lg:w-[750px] bg-white">
+      <div className="py-5 px-4 sm:p-5 mt-5 sm:mt-10 md:mt-10 lg:mt-15  w-[350px] sm:w-[350px] md:w-[550px]  min-h-[450px] lg:w-[750px] bg-white">
         <h1
           className="text-black font-sans italic flex justify-center text-center items-center text-[30px]"
           style={{ fontFamily: "Azonix" }}
